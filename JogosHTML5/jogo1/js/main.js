050ea345658cd06b5307afbf752662947ec21382
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-21 15:19:00 -0300

    Criado a função disparo

--- JogosHTML5/jogo1/js/main.js
+++ JogosHTML5/jogo1/js/main.js
@@ -13,7 +13,7 @@ function start() { // Inicio da função start()
 
 	var velocidade=5;
 	var posicaoY = parseInt(Math.random() * 334)
-
+	var podeAtirar=true;
 	var TECLA = {
 		W: 87,
 		S: 83,
@@ -76,7 +76,7 @@ function start() { // Inicio da função start()
 	
 		if (jogo.pressionou[TECLA.D]) {
 			
-			//Chama função Disparo	
+			disparo();
 		}
 
 	} // fim da função movejogador()
@@ -118,5 +118,35 @@ function start() { // Inicio da função start()
 		}
 
 	} // fim da função moveamigo()
+
+	function disparo() {
+	
+		if (podeAtirar==true) {
+		
+			podeAtirar=false;
+			
+			topo = parseInt($("#jogador").css("top"))
+			posicaoX= parseInt($("#jogador").css("left"))
+			tiroX = posicaoX + 190;
+			topoTiro=topo+37;
+			$("#fundoGame").append("<div id='disparo'></div");
+			$("#disparo").css("top",topoTiro);
+			$("#disparo").css("left",tiroX);
+			
+			var tempoDisparo=window.setInterval(executaDisparo, 30);
+		} //Fecha podeAtirar
+ 
+   		function executaDisparo() {
+		    posicaoX = parseInt($("#disparo").css("left"));
+		    $("#disparo").css("left",posicaoX+15); 
+
+	        	if (posicaoX>900) {			
+					window.clearInterval(tempoDisparo);
+					tempoDisparo=null;
+					$("#disparo").remove();
+					podeAtirar=true;	
+	            }
+		} // Fecha executaDisparo()
+	} // Fecha disparo()
 
 } // Fim da função start
